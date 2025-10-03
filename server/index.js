9230eb5afbe5f80b4251fb94e4ccf73bf63bf5f9
likhonsdev <likhonsdev@likhonsheikh.com>
2025-10-03 12:30:28 +0000

    feat: Configure project for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@
 const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
 app.use(cors());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/api/blogs', (req, res) => {
   fs.readFile('blogs.json', 'utf8', (err, data) => {
@@ -17,6 +21,12 @@ app.get('/api/blogs', (req, res) => {
     }
     res.send(data);
   });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*_response', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 app.listen(port, () => {
